0a28b4007d78f4e51b235acac304c45a1bebfd29
haseebahmed248 <haseebahmed6621@gmail.com>
2024-06-30 15:52:12 +0500

    improved data

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,21 +1,22 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import MainRouter from './Routes/MainRouter.js'
-dotenv.config()
-const app = express()
-app.use(express.json())
-app.use(cors({
-    origin:'http://localhost:5173',
-    origin:'https://code-fusion-backend.vercel.app/',
-    origin:'https://code-fusion-one.vercel.app/'
-}))
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import MainRouter from './Routes/MainRouter.js';
 
-app.use("/code",MainRouter);
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
+dotenv.config();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running ${process.env.PORT}`)
-})
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/code", MainRouter);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
